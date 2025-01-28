0489bbf258e01629d832ccf114d57c4ca6657f02
Tecnologia SIS Colombia <tecnologia@sis-colombia.com>
2025-01-27 20:44:51 -0500

    json strin quitar

--- src/app/components/eligibility/claim-entry/claim-entry.component.ts
+++ src/app/components/eligibility/claim-entry/claim-entry.component.ts
@@ -122,7 +122,7 @@ export class ClaimEntryComponent {
     previewClaim(): void {
         for (const row of this.rows) {
             if (row.dateInitial && row.dateFinal && row.dateFinal < row.dateInitial) {
-                this.msgService.warning(JSON.stringify('The final date cannot be earlier than the initial date'));
+                this.msgService.warning('The final date cannot be earlier than the initial date');
                 return;
             }
         }
